d7ad728554b6cac5ebbf17703784ba2801c77083
Fred Wenzel <fwenzel@mozilla.com>
2010-05-28 18:35:56 +0200

    JSON parsing magic, courtesy of jbalogh.

---
+++ product_details/__init__.py
@@ -0,0 +1,16 @@
+"""
+When this module is imported, we load all the .json files and insert them as
+module attributes using locals().  It's a magical and wonderful process.
+"""
+import json
+import os
+
+
+root = os.path.dirname(os.path.realpath(__file__))
+json_dir = os.path.join(root, 'json')
+
+for filename in os.listdir(json_dir):
+    if filename.endswith('.json'):
+        name = os.path.splitext(filename)[0]
+        path = os.path.join(json_dir, filename)
+        locals()[name] = json.load(open(path))

6f47d60f8047f64e93c7ad13562cebdf92610623
Fred Wenzel <fwenzel@mozilla.com>
2010-05-31 11:26:40 +0200

    setup.py, oh-so-shamelessly snagged from django-cache-machine

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+import product_details
+
+
+setup(
+    name='django-mozilla-product-details',
+    version=product_details.__version__,
+    description='Product and locale details for Mozilla products.',
+    long_description=open('README.md').read(),
+    author='Fred Wenzel',
+    author_email='fwenzel@mozilla.com',
+    url='http://github.com/fwenzel/django-mozilla-product-details',
+    license='BSD',
+    packages=['product_details'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)

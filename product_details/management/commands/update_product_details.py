f563980bada5a7c27cd1cdf4739a97969b666b2c
Fred Wenzel <fwenzel@mozilla.com>
2010-10-19 14:23:03 -0700

    Apparently, lchmod is not available on non-BSD unixes. Lame. Bug 605594.

--- product_details/management/commands/update_product_details.py
+++ product_details/management/commands/update_product_details.py
@@ -154,7 +154,7 @@ class Command(NoArgsCommand):
         tf.write(urllib2.urlopen(
             urljoin(self.PROD_DETAILS_URL, json_file)).read())
         tf.close()
-        os.lchmod(tf.name, 0644)
+        os.chmod(tf.name, 0644)
         shutil.move(tf.name, os.path.join(self.PROD_DETAILS_DIR, json_file))
 
         return True

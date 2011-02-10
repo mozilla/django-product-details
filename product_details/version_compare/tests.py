e1fc1ab49c0bfa0905489178107f50aa68cf079c
Fred Wenzel <fwenzel@mozilla.com>
2011-02-09 17:03:09 -0800

    Moved remaining version_compare tests from Inpt over.

--- product_details/version_compare/tests.py
+++ product_details/version_compare/tests.py
@@ -2,9 +2,12 @@
 Most of these tests are directly migrated from mozilla-central's reference
 implementation.
 """
+import copy
+
 from nose.tools import eq_
 
-from product_details.version_compare import Version, version_list
+from product_details.version_compare import (
+    Version, version_list, version_dict, version_int)
 
 
 # Versions to test listed in ascending order, none can be equal.
@@ -41,6 +44,7 @@ EQUALITY = (
 
 
 def test_version_compare():
+    """Test version comparison code, for parity with mozilla-central."""
     numlist = enumerate(map(lambda v: Version(v), COMPARISONS))
     for i, v1 in numlist:
         for j, v2 in numlist:
@@ -55,3 +59,49 @@ def test_version_compare():
     for v1 in equal_vers:
         for v2 in equal_vers:
             eq_(v1, v2)
+
+
+def test_simplify_version():
+    """Make sure version simplification works."""
+    versions = {
+        '4.0b1': '4.0b1',
+        '3.6': '3.6',
+        '3.6.4b1': '3.6.4b1',
+        '3.6.4build1': '3.6.4',
+        '3.6.4build17': '3.6.4',
+    }
+    for v in versions:
+        ver = Version(v)
+        eq_(ver.simplified, versions[v])
+
+
+def test_dict_vs_int():
+    """
+    version_dict and _int can use each other's data but must not overwrite
+    it.
+    """
+    version_string = '4.0b8pre'
+    dict1 = copy.copy(version_dict(version_string))
+    int1 = version_int(version_string)
+    dict2 = version_dict(version_string)
+    int2 = version_int(version_string)
+    eq_(dict1, dict2)
+    eq_(int1, int2)
+
+
+def test_version_list():
+    """Test if version lists are generated properly."""
+    my_versions = {
+        '4.0b2build8': '2010-12-06',
+        '3.0': '2010-12-01',
+        '4.0b1': '2010-11-24',
+        '4.0b2build7': '2010-12-05',
+    }
+    expected = ('4.0b2', '4.0b1')
+
+    test_list = version_list(my_versions, hide_below='4.0b1')
+
+    # Check if the generated version list is the same as we expect.
+    eq_(len(expected), len(test_list))
+    for n, v in enumerate(test_list):
+        eq_(v, expected[n])

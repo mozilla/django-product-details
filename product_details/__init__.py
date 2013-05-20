f2bb97595904c5c305fd3af28da47a7c8d53a10e
Paul McLanahan <pmac@mozilla.com>
2013-05-20 16:40:06 -0400

    Add ability to auto reload the JSON data from disk.

--- product_details/__init__.py
+++ product_details/__init__.py
@@ -8,18 +8,20 @@ import datetime
 import json
 import logging
 import os
+from os.path import getmtime, splitext
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 from product_details import settings_defaults
+from product_details.utils import memoize_for
 
 
 class MissingJSONData(IOError):
     pass
 
 
-__version__ = '0.6'
+__version__ = '0.7'
 __all__ = ['__version__', 'product_details', 'version_compare']
 
 log = logging.getLogger('product_details')
@@ -40,24 +42,83 @@ class ProductDetails(object):
     attributes, e.g.: product_details.firefox_version_history .
     """
     json_data = {}
+    json_mtimes = {}
+    json_dir = settings_fallback('PROD_DETAILS_DIR')
+    auto_reload = settings_fallback('PROD_DETAILS_AUTO_RELOAD')
+    auto_reload_regions = settings_fallback('PROD_DETAILS_AUTO_RELOAD_REGIONS')
 
-    def __init__(self):
+    def __init__(self, json_dir=None, auto_reload=None,
+                 auto_reload_regions=None):
         """Load JSON files and keep them in memory."""
+        if auto_reload is not None:
+            self.auto_reload = auto_reload
 
-        json_dir = settings_fallback('PROD_DETAILS_DIR')
+        if auto_reload_regions is not None:
+            self.auto_reload_regions = auto_reload_regions
 
-        for filename in os.listdir(json_dir):
+        if json_dir is not None:
+            self.json_dir = json_dir
+
+        # preload
+        for filename in os.listdir(self.json_dir):
             if filename.endswith('.json'):
-                name = os.path.splitext(filename)[0]
-                path = os.path.join(json_dir, filename)
-                self.json_data[name] = json.load(open(path))
+                self._load_json_data(splitext(filename)[0])
+
+    def _load_json_data(self, name):
+        """Load data from a JSON file"""
+        path = self._get_json_abspath(name)
+        with codecs.open(path, encoding='utf8') as fd:
+            self.json_data[name] = json.load(fd)
+            if self.auto_reload:
+                self.json_mtimes[name] = getmtime(path)
+
+    def _get_json_abspath(self, name):
+        """Return the full path to the JSON file for given name"""
+        return os.path.join(self.json_dir, '%s.json' % name)
+
+    @memoize_for(settings_fallback('PROD_DETAILS_AUTO_RELOAD_TTL'))
+    def _get_json_mtime(self, name):
+        path = self._get_json_abspath(name)
+        try:
+            mtime = getmtime(path)
+        except IOError:
+            mtime = None
+
+        return mtime
+
+    def _auto_reload_json_data(self, name):
+        mtime = self._get_json_mtime(name)
+        if mtime and mtime > self.json_mtimes.get(name):
+            try:
+                self._load_json_data(name)
+            except IOError:
+                pass
+
+    def _get_json_data(self, name, auto_reload=False):
+        """Accessor for JSON data."""
+        if name in self.json_data:
+            if auto_reload:
+                self._auto_reload_json_data(name)
+
+            return self.json_data[name]
+
+        try:
+            self._load_json_data(name)
+        except IOError:
+            pass
+
+        try:
+            return self.json_data[name]
+        except KeyError:
+            raise MissingJSONData('Requested product details '
+                                  'file %s not found!' % name)
 
     def __getattr__(self, key):
         """Catch-all for access to JSON files."""
         try:
-            return self.json_data[key]
-        except KeyError:
-            log.warn('Requested product details file %s not found!' % key)
+            return self._get_json_data(key, self.auto_reload)
+        except MissingJSONData as e:
+            log.warn(e.message)
             return collections.defaultdict(lambda: None)
 
     @property
@@ -93,14 +154,10 @@ class ProductDetails(object):
             lookup.insert(1, fallback)
         for l in lookup:
             key = 'regions/%s' % l
-            path = os.path.join(settings_fallback('PROD_DETAILS_DIR'),
-                                'regions', '%s.json' % l)
-            if self.json_data.get(key):
-                return self.json_data.get(key)
-            if os.path.exists(path):
-                with codecs.open(path, encoding='utf8') as fd:
-                    self.json_data[key] = json.load(fd)
-                    return self.json_data[key]
+            try:
+                return self._get_json_data(key, self.auto_reload_regions)
+            except MissingJSONData:
+                continue
 
         raise MissingJSONData('Unable to load region data for %s or en-US' %
                               locale)
--- product_details/settings_defaults.py
+++ product_details/settings_defaults.py
@@ -9,5 +9,12 @@ PROD_DETAILS_URL = 'http://svn.mozilla.org/libs/product-details/json/'
 # Target dir to drop JSON files into (must be writable)
 PROD_DETAILS_DIR = os.path.join(os.path.dirname(__file__), 'json')
 
+# Whether to check the modification time of the json files and reload
+# without needing to restart the server.
+PROD_DETAILS_AUTO_RELOAD = False
+PROD_DETAILS_AUTO_RELOAD_REGIONS = False
+# When auto_reload is on, only check the mtime once per TTL.
+PROD_DETAILS_AUTO_RELOAD_TTL = 600  # 10 minutes
+
 # log level.
 LOG_LEVEL = logging.INFO

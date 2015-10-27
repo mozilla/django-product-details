cdd6bc5258a21a1447c6313fad87056163b58a45
Julien Vehent <julien@linuxwall.info>
2015-10-27 18:03:00 -0400

    Use HTTPS when fetching JSON files

--- product_details/settings_defaults.py
+++ product_details/settings_defaults.py
@@ -4,7 +4,7 @@ import os
 
 # URL to clone product_details JSON files from.
 # Include trailing slash.
-PROD_DETAILS_URL = 'http://svn.mozilla.org/libs/product-details/json/'
+PROD_DETAILS_URL = 'https://svn.mozilla.org/libs/product-details/json/'
 
 # Target dir to drop JSON files into (must be writable)
 PROD_DETAILS_DIR = os.path.join(os.path.dirname(__file__), 'json')

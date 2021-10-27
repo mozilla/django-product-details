b268985dd4f34794210b3760dff3515b149ab49b
Steve Jalim <stevejalim@mozilla.com>
2021-10-27 16:27:30 +0100

    Bump version to 15.0.0 and extend Python support mentioned in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import codecs
 import os.path
 import re
+
 from setuptools import find_packages, setup
 
 
@@ -42,6 +43,10 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
